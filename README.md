62674bda5fe8bed7d5196f1147713932a9212407
Kristen Soroka <kristen.r.soroka@gmail.com>
2021-03-29 15:17:10 -0400

    Started tutorial - 2hours

---
+++ README.md
@@ -0,0 +1,39 @@
+# Snake Game Tutorial Follow along
+
+## Game Rules
+
+Logic of playing Snake
+    - snake eats apple and grows.
+    - can not run into itself or sides of board
+    - snake will move faster as game progress
+    - keep score
+
+
+## Notes
+
+Javascript Methods Used:
+
+1. .addEventListener()
+2. .querySelectorAll()
+3. .querySelector()
+4. .forEach()
+5. .clearInterval()
+7. .classList.add()
+8. .classList.remove()
+9. .keyCode
+10. .classList.contains() 
+11. .length
+12. Math.floor()
+13. Math.random()
+14. innerText
+15. setTimeout()
+16. .textContent
+17. .unshift()
+18. .pop()
+19. setInterval()
+20. .createElment()
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,36 @@
+const grid = document.querySelector('.grid')
+const startBtn = document.querySelector('#start')
+const score = document.querySelector('#score')
+let squares = []
+let currentSnake = [2,1,0]
+let direction = 1
+
+function createGrid(){                                  // Declaring the function
+    for ( let i = 0; i < 100; i++) {                    // Create 100 of these elements with a for loop
+        const square = document.createElement('div')    // Create element
+        //console.log(square)                             // Checking to see if the code is working
+        square.classList.add('square')                  // Add styling to the element 
+        grid.appendChild(square)                        // Append the child element to the parent; the element into our grid
+        squares.push(square)                            // Push each new square into squares array each you iterate thru the for loop
+    } 
+}
+
+createGrid()                                            // Invoke the function - without this nothing will happen!
+// console.log(squares)                                    // Cheacking to see the array worked
+
+currentSnake.forEach(index => squares[index].classList.add('snake')) //Passing currentSnake into squares array. Applies class to each index on the array. index can be anything as long as they are equal.  but index makes more sense here
+
+console.log("inital currentSnake: " + currentSnake)
+
+function move() {
+    const tail = currentSnake.pop()                     // Removing the last element from the currentSnake array; saving to a variable so later later use. 
+        console.log("index popped: " + tail)
+        console.log("currentSnake array after pop: " + currentSnake)
+    squares[tail].classList.remove('snake')             // Passing the popped off tail thru the squares array to remove the styling aka class of snake from the grid board
+    currentSnake.unshift(currentSnake[0] + direction)   // Adding square in direction we are heading.  Unshift methods adds to the beginning of an array
+        console.log("currentSnake array after unshift: " + currentSnake)
+        console.log(squares)                            // squares array now shows only two indices with class of snake
+    squares[currentSnake[0]].classList.add('snake')     // Adding styling by adding class of snake the new index which has a new square
+}
+
+move()
