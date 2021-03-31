3e4ce5003d88e90b170bd7184e7813f372e05835
Kristen Soroka <kristen.r.soroka@gmail.com>
2021-03-31 08:26:42 -0400

    keycodes javascript

--- README.md
+++ README.md
@@ -21,6 +21,10 @@ Javascript Methods Used:
 7. .classList.add()
 8. .classList.remove()
 9. .keyCode
+        // 39 is right arrow
+        // 38 is for the up arrow
+        // 37 is for the left arrow
+        // 40 is for the down arrow
 10. .classList.contains() 
 11. .length
 12. Math.floor()
@@ -31,7 +35,14 @@ Javascript Methods Used:
 17. .unshift()
 18. .pop()
 19. setInterval()
+    // const timerId = setInterval(function, time)  // time is in ms.  the larger the number the slower the movement, 500 v 1500
+    //clearInterval(timerId)
 20. .createElment()
+
+
+Statements used
+1. Do/While 
+    - first time seeing this in use!!  Notes to come
 
 
 
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const score = document.querySelector('#score')
 let squares = []
 let currentSnake = [2,1,0]
 let direction = 1
+let width = 10 
 
 function createGrid(){                                  // Declaring the function
     for ( let i = 0; i < 100; i++) {                    // Create 100 of these elements with a for loop
@@ -20,17 +21,41 @@ createGrid()                                            // Invoke the function -
 
 currentSnake.forEach(index => squares[index].classList.add('snake')) //Passing currentSnake into squares array. Applies class to each index on the array. index can be anything as long as they are equal.  but index makes more sense here
 
-console.log("inital currentSnake: " + currentSnake)
+//console.log("inital currentSnake: " + currentSnake)
 
 function move() {
     const tail = currentSnake.pop()                     // Removing the last element from the currentSnake array; saving to a variable so later later use. 
-        console.log("index popped: " + tail)
-        console.log("currentSnake array after pop: " + currentSnake)
+        //console.log("index popped: " + tail)
+        //console.log("currentSnake array after pop: " + currentSnake)
     squares[tail].classList.remove('snake')             // Passing the popped off tail thru the squares array to remove the styling aka class of snake from the grid board
     currentSnake.unshift(currentSnake[0] + direction)   // Adding square in direction we are heading.  Unshift methods adds to the beginning of an array
-        console.log("currentSnake array after unshift: " + currentSnake)
-        console.log(squares)                            // squares array now shows only two indices with class of snake
+        //console.log("currentSnake array after unshift: " + currentSnake)
+        //console.log(squares)                            // squares array now shows only two indices with class of snake
     squares[currentSnake[0]].classList.add('snake')     // Adding styling by adding class of snake the new index which has a new square
 }
 
 move()
+
+
+
+let timeID = setInterval(move, 1000)
+
+
+
+function control(e) {
+    if (e.keyCode === 39) {
+        direction = 1               // resetting 
+        // console.log('right arrow pressed')
+    } else if (e.keyCode === 38) {
+        direction = -width
+        // console.log('up arrow pressed')
+    } else if (e.keyCode === 37) {
+        direction = -1
+        // console.log('left arrow pressed')
+    } else if (e.keyCode === 40) {
+        direction = +width
+        // console.log('down arrow pressed')
+    }
+}
+
+document.addEventListener('keyup', control)   // ('event type', function)  see web for various eventypes
